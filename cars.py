82f57cb8cf554ab8abf2cd838d7c38408a496fa0
Isco170 <Francisco.Andre@TPGL.local>
2022-01-31 13:29:57 +0200

    Sort list temporarily

--- cars.py
+++ cars.py
@@ -1,3 +1,18 @@
 cars = ['bmw', 'audi', 'toyota', 'subaru']
-cars.sort()
+
+# Sort list permanently
+
+# cars.sort(reverse=True)
+# print(cars)
+
+# Sort list temporarily
+
+print('Here is the original list:')
 print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
+
