6f006bf9d930e8005b8fc5bb27f86e18c0a02874
Isco170 <Francisco.Andre@TPGL.local>
2022-01-31 11:38:33 +0200

    Adding elements to a list

--- motorcycles.py
+++ motorcycles.py
@@ -1,5 +1,15 @@
 motorcycles = ['honda', 'yamaha', 'suzuki']
+
+# Modifying element
+
+# print(motorcycles)
+# motorcycles[0] = 'ducati'
+# print(motorcycles)
+
+# Adding elements to a list
+
+print(motorcycles)
+motorcycles.append('ducati')
 print(motorcycles)
 
-motorcycles[0] = 'ducati'
-print(motorcycles)
+
