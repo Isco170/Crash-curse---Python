b01439811e41fd148ec1998477bd988c208242ce
Isco170 <Francisco.Andre@TPGL.local>
2022-01-31 13:41:32 +0200

    Finding the length of a list

--- cars.py
+++ cars.py
@@ -1,3 +1,6 @@
+from audioop import reverse
+
+
 cars = ['bmw', 'audi', 'toyota', 'subaru']
 
 # Sort list permanently
@@ -7,12 +10,20 @@ cars = ['bmw', 'audi', 'toyota', 'subaru']
 
 # Sort list temporarily
 
-print('Here is the original list:')
-print(cars)
+# print('Here is the original list:')
+# print(cars)
+# print("\nHere is the sorted list:")
+# print(sorted(cars))
+# print("\nHere is the original list again:")
+# print(cars)
 
-print("\nHere is the sorted list:")
-print(sorted(cars))
+# Printing a list in reverse order
 
-print("\nHere is the original list again:")
-print(cars)
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+# Finding the length of a list
+
+print(len(cars))
 
