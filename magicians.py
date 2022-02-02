afc7c386a284ca6edff5213cb8e4bbe8c545f666
Isco170 <Francisco.Andre@TPGL.local>
2022-02-02 14:35:15 +0200

    Doing more work within a for loop

--- magicians.py
+++ magicians.py
@@ -1,3 +1,4 @@
 magicians = ['alice', 'david', 'carolina']
 for magician in magicians:
     print(f"{magician.title()}, that was a  great trick!")
+    print(f"I can't wait ro see your next trick, {magician.title()}.\n")
