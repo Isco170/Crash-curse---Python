10d9ff04b1dab69911799194ecd7f868243e33ca
Isco170 <Francisco.Andre@TPGL.local>
2022-02-02 15:26:36 +0200

    List comprehensions

--- squares.py
+++ squares.py
@@ -1,6 +1,9 @@
-squares = []
-for value in range(1, 11):
-    square = value ** 2
-    squares.append(square)
+# squares = []
+# for value in range(1, 11):
+#     square = value ** 2
+#     squares.append(square)
 
+# List comprehensions
+
+squares = [value**2 for value in range(1,11)]
 print(squares)
