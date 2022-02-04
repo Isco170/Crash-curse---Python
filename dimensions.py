329f6bce59381fa215ec99035c133979bd17ca33
Isco170 <Francisco.Andre@TPGL.local>
2022-02-04 09:36:07 +0200

    writing over a tuple

---
+++ dimensions.py
@@ -0,0 +1,12 @@
+dimensions = (200, 50)
+
+# Writing over a tuple
+
+print("Original dimensions:")
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+print("\nModified dimensions:")
+for dimension in dimensions:
+    print(dimension)
