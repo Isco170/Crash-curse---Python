57061d5e9d2469393a0f6ff36c2b9f3d3d693791
Isco170 <Francisco.Andre@TPGL.local>
2022-02-07 09:17:42 +0200

    Modifying values in a dictionary

--- 06 DICTIONARIES/alien.py
+++ 06 DICTIONARIES/alien.py
@@ -3,6 +3,13 @@ alien_0 = {'color': 'green', 'points': 5}
 print(alien_0)
 
 # Adding new key-value pairs
-alien_0['x_position'] = 0
-alien_0['y_position'] = 25
-print(alien_0)
+# alien_0['x_position'] = 0
+# alien_0['y_position'] = 25
+# print(alien_0)
+
+# Modifying values in a dictionary
+print(f"The alien is {alien_0['color']}.")
+
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}.")
+
