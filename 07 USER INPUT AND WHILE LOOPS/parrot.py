fba63dc4874290f9f2e849b741b3e2e86ef2cb57
Isco170 <Francisco.Andre@TPGL.local>
2022-02-08 10:18:13 +0200

    Using a flag

--- 07 USER INPUT AND WHILE LOOPS/parrot.py
+++ 07 USER INPUT AND WHILE LOOPS/parrot.py
@@ -2,11 +2,21 @@
 # print(message)
 
 # Letting the user choose when to quit
-
 prompt = "\nTell me something, and I will repeat it back to you:"
 prompt += "\nEnter 'quit' to end the program. "
 
 message = ""
-while message != 'quit':
+# while message != 'quit':
+#     message = input(prompt)
+#     print(message)
+
+# Using a flag
+active = True
+while active:
     message = input(prompt)
-    print(message)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
+
