22e9414427418d869e15770e03e9713523ec864b
Isco170 <Francisco.Andre@TPGL.local>
2022-02-08 10:29:37 +0200

    Using continue in a loop

--- 07 USER INPUT AND WHILE LOOPS/counting.py
+++ 07 USER INPUT AND WHILE LOOPS/counting.py
@@ -1,4 +1,13 @@
-current_number = 1
-while current_number <= 5:
-    print(current_number)
-    current_number += 1
+# current_number = 1
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
+
+
+# Using continue in a loop
+current_numer = 0
+while current_numer < 10:
+    current_numer += 1
+    if current_numer % 2 == 0:
+        continue
+    print(current_numer)
