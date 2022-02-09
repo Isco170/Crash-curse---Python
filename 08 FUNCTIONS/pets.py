c204ec4059d68ee805a123710b48052ee22350af
Isco170 <Francisco.Andre@TPGL.local>
2022-02-09 10:45:34 +0200

    Default values

--- 08 FUNCTIONS/pets.py
+++ 08 FUNCTIONS/pets.py
@@ -1,12 +1,18 @@
 
-def describe_pet(animal_type, pet_name):
-    print(f"\nI have a {animal_type}.")
-    print(f"My {animal_type}'s name is {pet_name.title()}.")
+# def describe_pet(animal_type, pet_name):
+#     print(f"\nI have a {animal_type}.")
+#     print(f"My {animal_type}'s name is {pet_name.title()}.")
 
 # Positional arguments
 # describe_pet('hamster', 'harry')
 
 # Keyword arguments
-describe_pet(animal_type='hamster', pet_name='harry')
+# describe_pet(animal_type='hamster', pet_name='harry')
 
+# Default values
+def describe_pet(pet_name, animal_type='dog'):
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(pet_name='willie')
 
