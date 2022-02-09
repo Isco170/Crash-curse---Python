ae110b89424b3605c2900aa2550374dfd1badbf6
Isco170 <Francisco.Andre@TPGL.local>
2022-02-09 10:21:40 +0200

    Passing information to a function

--- 08 FUNCTIONS/greeter.py
+++ 08 FUNCTIONS/greeter.py
@@ -1,4 +1,10 @@
-def greet_user():
-    print("Hello!")
+# def greet_user():
+#     print("Hello!")
 
-greet_user()
+# greet_user()
+
+# Passing information to a function
+def greet_user(username):
+    print(f"Hello, {username.title()}!")
+
+greet_user('Isco')
