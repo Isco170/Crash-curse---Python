39f88a2d477bcbd1a35ee14acadb27852f231a5a
Isco170 <Francisco.Andre@TPGL.local>
2022-02-21 09:04:39 +0200

    Making an argument optional

--- 08 FUNCTIONS/formatted_names.py
+++ 08 FUNCTIONS/formatted_names.py
@@ -1,6 +1,9 @@
-def get_formatted_name(first_name, last_name):
-    full_name = f"{first_name} {last_name}"
+def get_formatted_name(first_name, last_name, middle_name = ''):
+    if middle_name:
+        full_name = f"{first_name} {middle_name} {last_name}"
+    else:
+        full_name = f"{first_name} {last_name}"
     return full_name.title()
 
-musician = get_formatted_name('jimi', 'hendrix')
+musician = get_formatted_name('john', 'hooker')
 print(musician)
