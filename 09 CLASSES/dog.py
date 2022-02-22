cea499e0cbfed35d2e4fda6c24130623de18f7df
Isco170 <Francisco.Andre@TPGL.local>
2022-02-22 09:57:26 +0200

    Creating clas Dog

--- 09 CLASSES/dog.py
+++ 09 CLASSES/dog.py
@@ -1,9 +1,10 @@
-def __init__(self, name, age):
-    self.name = name
-    self.age = age
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-def sit(self):
-    print(f"{self.name} is now sitting.")
+    def sit(self):
+        print(f"{self.name} is now sitting.")
 
-def roll_over(self):
-    print(f"{self.name} rolled over!")
+    def roll_over(self):
+        print(f"{self.name} rolled over!")
