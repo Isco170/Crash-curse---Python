68368abe2cfac1e5457733dfa66d2b13c8f67488
Isco170 <Francisco.Andre@TPGL.local>
2022-02-22 11:35:44 +0200

    Defining attribute and methods for the child class

--- 09 CLASSES/electric_car.py
+++ 09 CLASSES/electric_car.py
@@ -24,6 +24,11 @@ class Car:
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.baterry_size = 75
+
+    def describe_battery(self):
+        print(f"This car has a {self.baterry_size}-kwh battery.")
 
 my_tesla = ElectricCar('testa', 'model s', 2019)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
