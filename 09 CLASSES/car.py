bc6edce6d76f1f786f850d3687abde65de59b293
Isco170 <Francisco.Andre@TPGL.local>
2022-02-22 11:57:18 +0200

    Importing a single class

--- 09 CLASSES/car.py
+++ 09 CLASSES/car.py
@@ -22,11 +22,11 @@ class Car:
         self.odometer_reading += miles
 
     
-my_used_car = Car('subaru', 'outback', 2015)
-print(my_used_car.get_descriptive_name())
+# my_used_car = Car('subaru', 'outback', 2015)
+# print(my_used_car.get_descriptive_name())
 
-my_used_car.update_odometer(23_500)
-my_used_car.read_odometer()
+# my_used_car.update_odometer(23_500)
+# my_used_car.read_odometer()
 
-my_used_car.increment_odometer(100)
-my_used_car.read_odometer()
+# my_used_car.increment_odometer(100)
+# my_used_car.read_odometer()
