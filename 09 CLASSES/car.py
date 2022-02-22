5ebdad774230583a03518a0c5dfd5e1cc01810c8
Isco170 <Francisco.Andre@TPGL.local>
2022-02-22 10:55:13 +0200

    Modifying an attribute's value through a method

--- 09 CLASSES/car.py
+++ 09 CLASSES/car.py
@@ -12,9 +12,11 @@ class Car:
     def read_odometer(self):
         print(f"This car has {self.odometer_reading} miles on it.")
     
+    def update_odometer(self, milage):
+        self.odometer_reading = milage
+    
 my_new_car = Car('Audi', 'a4', 2019)
 print(my_new_car.get_descriptive_name())
 
-my_new_car.odometer_reading = 23
+my_new_car.update_odometer(23)
 my_new_car.read_odometer()
-        
