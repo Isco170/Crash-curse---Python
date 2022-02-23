8c3e4f2cde85959536e63a9adc6cf45f1ae1c126
Isco170 <Francisco.Andre@TPGL.local>
2022-02-23 13:52:10 +0200

    Appending to a file

--- 10 FILES AND EXCEPTIONS/write_message.py
+++ 10 FILES AND EXCEPTIONS/write_message.py
@@ -1,5 +1,10 @@
 filename = './10 FILES AND EXCEPTIONS/programming.txt'
 
-with open(filename, 'w') as file_object:
-    file_object.write('I love programming.\n')
-    file_object.write("I love creating new games.\n")
+# with open(filename, 'w') as file_object:
+#     file_object.write('I love programming.\n')
+#     file_object.write("I love creating new games.\n")
+
+# Appending to a file
+with open(filename, 'a') as file_object:
+    file_object.write('I also love finding meaning in large datasets.\n')
+    file_object.write("I love creating apps that can run in a browser.\n")
