c13e55a2c3ca9efc93afc61badc860c7b01fb0ef
Isco170 <Francisco.Andre@TPGL.local>
2022-02-23 15:48:56 +0200

    Refactoring

--- 10 FILES AND EXCEPTIONS/remember_me.py
+++ 10 FILES AND EXCEPTIONS/remember_me.py
@@ -1,17 +1,25 @@
 import json
 
-def greet_user():
+def get_stored_username():
     filename = './10 FILES AND EXCEPTIONS/username.json'
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
+        return None
+    else:
+        return username
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
         username = input("What is your name? ")
+        filename = './10 FILES AND EXCEPTIONS/username.json'
         with open(filename, 'w') as f:
             json.dump(username, f)
             print(f"We'll remember you when you come back, {username}!")
-    else:
-        print(f"Welcome back, {username}!")
 
 greet_user()
 
