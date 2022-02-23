07d949406ef489ae955ca40b5121bbc606ea125a
Isco170 <Francisco.Andre@TPGL.local>
2022-02-23 10:07:13 +0200

    Reading an entire file done

--- 10 FILES AND EXCEPTIONS/file_reader.py
+++ 10 FILES AND EXCEPTIONS/file_reader.py
@@ -1,3 +1,8 @@
-with open('./10 FILES AND EXCEPTIONS/pi_digits.txt') as file_object:
-    contents = file_object.read()
-print(contents)
+# with open('./10 FILES AND EXCEPTIONS/pi_digits.txt') as file_object:
+#     contents = file_object.read()
+# print(contents)
+
+filename = './10 FILES AND EXCEPTIONS/pi_digits.txt'
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
