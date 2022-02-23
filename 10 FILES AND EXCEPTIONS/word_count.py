b2b3d0573b8257026d38374a9d2acf7b6664b249
Isco170 <Francisco.Andre@TPGL.local>
2022-02-23 15:01:49 +0200

    Working with multiple files

---
+++ 10 FILES AND EXCEPTIONS/word_count.py
@@ -0,0 +1,16 @@
+def count_words(filename):
+    try:
+        with open(filename, encoding='utf-8') as f:
+            contents = f.read()
+    except FileNotFoundError:
+        print(f"Sorry, the file {filename} does not exist.")
+    else:
+        words = contents.split()
+        num_words = len(words)
+        print(f"The file {filename} has about {num_words} words.")
+
+
+filenames = ['./10 FILES AND EXCEPTIONS/alice.txt', './10 FILES AND EXCEPTIONS/siddhartha.txt', './10 FILES AND EXCEPTIONS/moby_dick.txt', './10 FILES AND EXCEPTIONS/little_women.txt']
+
+for filename in filenames:
+    count_words(filename)
