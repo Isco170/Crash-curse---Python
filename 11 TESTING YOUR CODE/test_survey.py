d54ec79adab3ef6a7ed6360561ec4b7553237f39
Isco170 <Francisco.Andre@TPGL.local>
2022-02-28 09:47:41 +0200

    Testing the AnonymousSurvey class

--- 11 TESTING YOUR CODE/test_survey.py
+++ 11 TESTING YOUR CODE/test_survey.py
@@ -3,10 +3,20 @@ from survey import AnnoymousSurvey
 
 class TestAnonymousSurvey(unittest.TestCase):
     def test_store_sigle_response(self):
-        question = "What language did you first learn to speak? "
+        question = "What language did you first learn to speak?"
         my_survey = AnnoymousSurvey(question)
         my_survey.store_response('English')
         self.assertIn('English', my_survey.responses)
+
+    def test_store_three_responses(self):
+        question = "What language did you first learn to speak?"
+        my_survey = AnnoymousSurvey(question)
+        responses = ['English', 'Spanish', 'Mandarin']
+        for response in responses:
+            my_survey.store_response(response)
+        
+        for response in responses:
+            self.assertIn(response, my_survey.responses)
 
 if __name__ == '__main__':
     unittest.main()
