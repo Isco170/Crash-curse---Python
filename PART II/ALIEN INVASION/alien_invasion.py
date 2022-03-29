342afbc68e058ed132d987e7e9a98ac9568ff411
Isco170 <Francisco.Andre@TPGL.local>
2022-03-29 14:18:59 +0200

    Ajustes

--- PART II/ALIEN INVASION/alien_invasion.py
+++ PART II/ALIEN INVASION/alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -16,6 +18,9 @@ class AlienInvasion:
         self.settings.screen_width = self.screen.get_rect().width
         self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
+
+        # Create an instance to store game statitics
+        self.stats = GameStats(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
